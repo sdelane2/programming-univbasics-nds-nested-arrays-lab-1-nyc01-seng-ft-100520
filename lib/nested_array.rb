63833b134d97564abcb1f14f8f513c80f6280766
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-19 19:30:25 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,8 +37,7 @@ def sorted_matrix
     ORGANIC_PRODUCE
     ]
   
-  produce_storage_room[0].sort
-  produce_storage_room[1].sort
+  produce_storage_room[0].sort(produce_storage_room[1].sort)
   
 end
 
